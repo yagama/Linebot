11cbd9c84c84b12ff20e813bf84abe3de87f46ba
Rick <yagama@gmail.com>
2019-01-09 22:25:44 +0800

    Python training

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-import requests
-import re
 import random
 import configparser
-from bs4 import BeautifulSoup
 from flask import Flask, request, abort
-import urllib.request
-import urllib.parse
-import json
-# from imgurpython import ImgurClient
+from googleapiclient.discovery import build
+
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -23,6 +18,9 @@ config.read("config.ini")
 
 line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
 handler = WebhookHandler(config['line_bot']['Channel_Secret'])
+DEVELOPER_KEY = 'AIzaSyDKQzkRv8pPGJINkaZ-I5YbjpJNeNyPngk'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -42,265 +40,29 @@ def callback():
 
     return 'ok'
 
-def apple_news():
-    target_url = 'https://tw.appledaily.com/new/realtime'
- #   print('Start parsing appleNews....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
+def youtube_search(options):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(q=options, part='id,snippet', maxResults=11).execute()
     content = ""
-    for index, data in enumerate(soup.select('.rtddt a'), 0):
-        if index == 5:
-            return content
-        link = data['href']
-        content += '{}\n\n'.format(link)
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            content += '{}\nhttps://youtu.be/{}\n\n'.format(search_result['snippet']['title'], search_result['id']['videoId'])
+
     return content
 
-
-def get_page_number(content):
-    start_index = content.find('index')
-    end_index = content.find('.html')
-    page_number = content[start_index + 5: end_index]
-    return int(page_number) + 1
-
-
-def craw_page(res, push_rate):
-    soup_ = BeautifulSoup(res.text, 'html.parser')
-    article_seq = []
-    for r_ent in soup_.find_all(class_="r-ent"):
-        try:
-            # 先得到每篇文章的篇url
-            link = r_ent.find('a')['href']
-            if link:
-                # 確定得到url再去抓 標題 以及 推文數
-                title = r_ent.find(class_="title").text.strip()
-                rate = r_ent.find(class_="nrec").text
-                url = 'https://www.ptt.cc' + link
-                if rate:
-                    rate = 100 if rate.startswith('爆') else rate
-                    rate = -1 * int(rate[1]) if rate.startswith('X') else rate
-                else:
-                    rate = 0
-                # 比對推文數
-                if int(rate) >= push_rate:
-                    article_seq.append({
-                        'title': title,
-                        'url': url,
-                        'rate': rate,
-                    })
-        except Exception as e:
-            # print('crawPage function error:',r_ent.find(class_="title").text.strip())
-            print('本文已被刪除', e)
-    return article_seq
-
-
-def crawl_page_gossiping(res):
-    soup = BeautifulSoup(res.text, 'html.parser')
-    article_gossiping_seq = []
-    for r_ent in soup.find_all(class_="r-ent"):
-        try:
-            # 先得到每篇文章的篇url
-            link = r_ent.find('a')['href']
-
-            if link:
-                # 確定得到url再去抓 標題 以及 推文數
-                title = r_ent.find(class_="title").text.strip()
-                url_link = 'https://www.ptt.cc' + link
-                article_gossiping_seq.append({
-                    'url_link': url_link,
-                    'title': title
-                })
-
-        except Exception as e:
-            # print u'crawPage function error:',r_ent.find(class_="title").text.strip()
-            # print('本文已被刪除')
-            print('delete', e)
-    return article_gossiping_seq
-
-def ptt_beauty():
-    rs = requests.session()
-    res = rs.get('https://www.ptt.cc/bbs/Beauty/index.html', verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    all_page_url = soup.select('.btn.wide')[1]['href']
-    start_page = get_page_number(all_page_url)
-    page_term = 3  # crawler count
-    push_rate = 20  # 推文
-    index_list = []
-    article_list = []
-    for page in range(start_page, start_page - page_term, -1):
-        page_url = 'https://www.ptt.cc/bbs/Beauty/index{}.html'.format(page)
-        index_list.append(page_url)
-
-    # 抓取 文章標題 網址 推文數
-    while index_list:
-        index = index_list.pop(0)
-        res = rs.get(index, verify=False)
-        # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
-        if res.status_code != 200:
-            index_list.append(index)
-            # print u'error_URL:',index
-            # time.sleep(1)
-        else:
-            article_list = craw_page(res, push_rate)
-            # print u'OK_URL:', index
-            # time.sleep(0.05)
-    content = '三頁內超過20推的有\n'
-    for article in article_list:
-        data = '[{} push] {}\n{}\n\n'.format(article.get('rate', None), article.get('title', None),
-                                             article.get('url', None))
-        content += data
-    return content
-
-
-def ptt_hot():
-    target_url = 'http://disp.cc/b/PttHot'
-    print('Start parsing pttHot....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for data in soup.select('#list div.row2 div span.listTitle'):
-        title = data.text
-        link = "http://disp.cc/b/" + data.find('a')['href']
-        if data.find('a')['href'] == "796-59l9":
-            break
-        content += '{}\n{}\n\n'.format(title, link)
-    return content
-
-def movie():
-    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
-    print('Start parsing movie ...')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for index, data in enumerate(soup.select('ul.filmNextListAll a')):
-        if index == 10:
-            return content
-        title = data.text.replace('\t', '').replace('\r', '')
-        link = "http://www.atmovies.com.tw" + data['href']
-        content += '{}\n{}\n'.format(title, link)
-    return content
-
-def oil_price():
-    target_url = 'https://gas.goodlife.tw/'
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    title = soup.select('#main')[0].text.replace('\n', '').split('(')[0]
-    gas_price = soup.select('#gas-price')[0].text.replace('\n\n\n', '').replace(' ', '')
-    cpc = soup.select('#cpc')[0].text.replace(' ', '')
-    content = '{}\n{}{}'.format(title, gas_price, cpc)
-    return content
-
-def avgle(event):
-	AVGLE_SEARCH_VIDEOS_API_URL = 'https://api.avgle.com/v1/jav/{}/{}?limit={}'
-	query = event
-	page = 0
-	limit = 5
-	response = json.loads(urllib.request.urlopen(AVGLE_SEARCH_VIDEOS_API_URL.format(urllib.parse.quote_plus(query), page, limit)).read().decode())
-	#print(response)
-	content = ""
-	if response['success']:
-		for i in range(0,len(response["response"]["videos"])):
-			content += '{}\n{}\n\n'.format(response["response"]["videos"][i]['title'][0:20], response["response"]["videos"][i]['embedded_url'])
-	return content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
- #   print("event.reply_token:", event.reply_token)
- #   print("event.message.text:", event.message.text)
 
-    if event.message.text == "蘋果即時":
-        content = apple_news()
+    if event.message.text[0:3] == "YT ":
+        content = youtube_search(event.message.text[3:])
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
         return 0
-    if event.message.text == "表特":
-        content = ptt_beauty()
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=content))
-        return 0
-    if event.message.text == "PTT熱門":
-        content = ptt_hot()
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=content))
-        return 0
-    if event.message.text == "電影":
-        content = movie()
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=content))
-        return 0
-    if event.message.text == "油價查詢":
-        content = oil_price()
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=content))
-        return 0
-
-    if event.message.text[0:3] == "Av ":
-        content = avgle(event.message.text[3:])
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=content))
-        return 0
-'''
-	carousel_template_message = TemplateSendMessage(
-		alt_text='目錄 template',
-		template=CarouselTemplate(
-			columns=[
-				CarouselColumn(
-					thumbnail_image_url='https://i.imgur.com/hd9gJ2v.jpg?1',
-					title='Hi',
-					text='今天好嗎',
-					actions=[
-						MessageAction(
-							label='PTT熱門',
-							text='PTT熱門'
-						),
-						URIAction(
-							label='影片',
-							uri='https://youtu.be/'
-						),
-						URIAction(
-							label='Github',
-							uri='https://github.com/'
-						)
-					]
-				),
-				CarouselColumn(
-	#                    thumbnail_image_url='https://i.imgur.com/DDAm8pC.png',
-					thumbnail_image_url='https://i.imgur.com/IPwI5Q1.jpg',
-					title='我看看',
-					text='還有什麼',
-					actions=[
-						MessageAction(
-							label='蘋果即時',
-							text='蘋果即時'
-						),
-						MessageAction(
-							label='油價查詢',
-							text='油價查詢'
-						),
-						URIAction(
-							label='Python Spec',
-							uri='https://docs.python.org/3/py-modindex.html'
-						)
-					]
-				),                
-			]
-		)
-	)
-	line_bot_api.reply_message(event.reply_token, carousel_template_message)
-'''
-
-
-
+		
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
     print("package_id:", event.message.package_id)
